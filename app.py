1de6eedb5f805b7bd42af0882e152659fbb981d5
vishnuklu <cmvishnubabu08@gmail.com>
2025-04-16 22:36:27 +0530

    Simulate success

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Simulating an error (divide by zero)
-    result = 1 / 0  # This will cause a ZeroDivisionError
+    result = 20 / 5  # This will cause a ZeroDivisionError
     return f"Result: {result}"
 
 if __name__ == "__main__":
